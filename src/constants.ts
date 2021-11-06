55adeb1bf5b3b56f702ba57d26396e48b0423063
amp <ampanahi20@gmail.com>
2021-11-07 01:16:30 +0330

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,66 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 56,
+  BSCTESTNET = 97,
+  MATICMAINNET = 137,
+  MATICTESTNET = 80001,
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export interface FactoryProps {
+  56: string;
+  97: string;
+  137: string;
+  80001: string;
+}
+
+export const FACTORY_ADDRESS : FactoryProps = {
+  [ChainId.MAINNET]: '0x2C2733103e796e27184b461432Ac2c8f08ff61d3',
+  [ChainId.BSCTESTNET]: '0x3c0164189EFc4F73d3e73c6780D9e1c9C5aD800c',
+  [ChainId.MATICMAINNET]: '0x4b01054bd4cDa97A6D8d048a5Ea774210b11B6b2',
+  [ChainId.MATICTESTNET]: '0x4b01054bd4cDa97A6D8d048a5Ea774210b11B6b2',
+}
+
+export const INIT_CODE_HASH : FactoryProps = {
+  [ChainId.MAINNET]: '0x4d9d718bb9cf785650f142f8e2f5f042b78fb13e9d470347e6b6514c0a10549b',
+  [ChainId.BSCTESTNET]: '0x4d9d718bb9cf785650f142f8e2f5f042b78fb13e9d470347e6b6514c0a10549b',
+  [ChainId.MATICMAINNET]: '0x4d9d718bb9cf785650f142f8e2f5f042b78fb13e9d470347e6b6514c0a10549b',
+  [ChainId.MATICTESTNET]: '0x4d9d718bb9cf785650f142f8e2f5f042b78fb13e9d470347e6b6514c0a10549b',
+}
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _998 = JSBI.BigInt(998)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
